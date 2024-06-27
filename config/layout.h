50e5939daa6ef21cc534cae09b3ec45d6d482e81
Vlada <vladislav.trefil@protonmail.com>
2024-06-27 20:54:39 +0200

    test

--- config/layout.h
+++ config/layout.h
@@ -3,13 +3,13 @@
 #define U_STRINGIFY(x) #x
 
 #define U_MACRO(name,...) \
-/ { \
-  macros { \
-    name: name { \
-      compatible = "zmk,behavior-macro"; \
-      #binding-cells = <0>; \
-      bindings = <BINDING>, <SHIFT_BINDING>; \
-      __VA_ARGS__ \
-    }; \
-  }; \
-};
+    / { \
+      macros { \
+        name: name { \
+          label = U_STRINGIFY(ZM_ ## name); \
+          compatible = "zmk,behavior-macro"; \
+          #binding-cells = <0>; \
+          __VA_ARGS__ \
+        }; \
+      }; \
+    };
